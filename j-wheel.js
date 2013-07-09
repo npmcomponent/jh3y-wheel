9538094ffabd4c1f2c79f785a187eae9c33b8def
jh3y <jhey.tompkins@gmail.com>
2013-07-09 19:45:05 +0100

    Added hours and minutes to demo.

--- j-wheel.js
+++ j-wheel.js
@@ -26,7 +26,7 @@
 					content: $('<div>3</div>')
 				}
 			],
-			verticalAxis: true
+			verticalAxis: false
 		},
 			$wheel = $(this.selector),
 			rotationDegree,
@@ -37,11 +37,74 @@
 				$DOMElement.addClass('wheel')
 					.css({'width': options.width, 'height': options.height});
 				//in here we need to iterate over the panels or panel content and put it in I think.
-				if (options.panelsContainer) {
-					//this becomes quite difficult because we can make it so the user doesn't have to do much but then we have to maybe iterate through the child elements or something
-					//and use the dom elements which are assigned panel class? maybe just find all div underneath
-					//investigate the container for divs and assign them as the panels and take any data-wheel-value attributes into consideration possibly.
-					console.log('TODO: you havent implemented this technique yet');
+				if (options.panelContainer) {
+					var $panelContainer
+					if ($wheel.children('div').length > 1) {
+						if (options.verticalAxis) {
+							$panelContainer = $('<div class="panel-container" style="transform: translateZ( -' + translation + 'px ) rotateX( -0deg );"></div>');
+						} else {
+							$panelContainer = $('<div class="panel-container" style="transform: translateZ( -' + translation + 'px ) rotateY( -0deg );"></div>');
+						}
+						var $panels = $wheel.children('div');
+						$wheel.append($panelContainer.append($panels));
+					} else if ($wheel.children().length === 1) {
+						$panelContainer = $wheel.children('div').first();
+						$panelContainer.addClass('panel-container');
+						if (options.verticalAxis) {
+							$panelContainer.attr('style', 'transform: translateZ( -' + translation + 'px ) rotateX( -0deg );');
+						} else {
+							$panelContainer.attr('style', 'transform: translateZ( -' + translation + 'px ) rotateY( -0deg );');
+						}
+					} else {
+						console.log('WHEELJS: No content to be displayed');
+					}
+					//by time we get here we have the DOM structure we just need to configure the panels and we are done.
+					$.each($panels, function (key) {
+							var degs = (360 / $panels.length) * key;
+							var panelData = this,
+								$panel;
+							// debugger;
+							if (typeof(panelData.content) === 'string') {
+								$panel = $(panelData.content);
+							} else if (typeof(panelData.content) === 'object') {
+								$panel = panelData.content;
+							} else {
+								$panel = $(panelData);
+							}
+							if (panelData.value) {
+								$panel.attr('data-wheel-value', panelData.value);
+							} else if ($panel.attr('value')) {
+								$panel.attr('data-wheel-value', $panel.attr('value'));
+							}
+							$panel.addClass('panel');
+							if (options.verticalAxis) {
+								$panel.attr('style', 'transform: rotateX(' + degs + 'deg) translateZ(' + translation + 'px)');	
+							} else {
+								$panel.attr('style', 'transform: rotateY(' + degs + 'deg) translateZ(' + translation + 'px)');
+							}
+							if (options.click) {
+								if (options.verticalAxis) {
+									// top and bottom click functionality
+									var $top = $('<div class="panel-top"></div>').on('click', function () {
+										roll('top');
+									});
+									var $bottom = $('<div class="panel-bottom"></div>').on('click', function () {
+										roll('bottom');
+									});
+									$panel.append($top, $bottom);
+								} else {
+									var $left = $('<div class="panel-left"></div>').on('click', function () {
+										roll('left');
+									});
+									var $right = $('<div class="panel-right"></div>').on('click', function () {
+										roll('right');
+									});
+									$panel.append($left, $right);
+								}
+							}
+							// $panelContainer.append($panel);
+						});
+
 				} else if (options.panels) {
 					if (typeof(options.panels) === 'object' && options.panels.length > 0) {						
 						var $panelContainer
@@ -153,7 +216,17 @@
 			this.currentRotation = currentRotation = 0;
 			var panelLength = options.panels.length;
 			if (options.panelContainer) {
-				this.find('div').length;
+				if (this.children('div').length > 1) {
+					panelLength = this.children('div').length;
+				} else if (this.children('div').length === 1) {
+					if (this.children('div').first().children('div').length > 0) {
+						panelLength = this.children('div').first().children('div').length;
+					} else {
+						panelLength = 1;
+					}
+				}
+			} else if (options.panelLength) {
+				panelLength = options.panelLength;
 			}
 			this.rotationDegree = rotationDegree = 360 / panelLength;
 			this.translation = translation = Math.round( ( options.panelWidth / 2 ) / 
